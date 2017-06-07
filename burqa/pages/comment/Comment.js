17d29d239cf539143fbe703aa10fcbd74ca576e8
Luthor <luxiao418@pingan.com.cn>
2017-06-07 10:01:51 +0800

    commit,增加评论部分代码

---
+++ burqa/pages/comment/Comment.js
@@ -0,0 +1,49 @@
+/**
+ * Created by luthor on 07/06/2017.
+ */
+import React, {
+    View
+} from 'react'
+import FeedsCategoryBar from "../../../../src/components/FeedsCategoryBar";
+import NewCommentList from "./NewCommentList";
+import HotCommentList from "./HotCommentList";
+
+const titles = ['最新评论', '热门评论'];
+const controllers = [
+    {categoryId: 1, controller: NewCommentList},
+    {categoryId: 2, controller: HotCommentList},
+]
+
+/**
+ * @Description 评论
+ * @Author lucio
+ * @Email lucio0314@163.com
+ * @Date 07/06/2017 09:49
+ * @Version 1.0.0
+ */
+export default class Comment extends Component {
+
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                <ScrollableTabView
+                    renderTabBar={() => <FeedsCategoryBar tabNames={titles}/>}
+                    tabBarPosition='top'
+                    scrollWithoutAnimation={false}
+                >
+                    {controllers.map((data, index) => {
+                        let Component = data.controller;
+                        return (
+                            <Component
+                                key={titles[index]}
+                                tabLabel={titles[index]}
+                                categoryId={data.categoryId}
+                                // navigator={navigator}
+                            />
+                        )
+                    })}
+                </ScrollableTabView>
+            </View>
+        )
+    }
+}
---
+++ burqa/pages/comment/NewCommentList.js
@@ -0,0 +1,22 @@
+/**
+ * Created by luthor on 07/06/2017.
+ */
+import React, {Component} from 'react'
+
+/**
+ * @Description 评论列表
+ * @Author lucio
+ * @Email lucio0314@163.com
+ * @Date 07/06/2017 09:49
+ * @Version 1.0.0
+ */
+export default class NewCommentList extends Component {
+
+    render() {
+        return (
+            <View style={{flex: 1}}>
+               
+            </View>
+        )
+    }
+}
