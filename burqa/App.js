8c9000f1f2a7940a45548c63a6aaf2019c81add0
Luthor <luxiao418@pingan.com.cn>
2017-06-09 14:08:34 +0800

    commit

--- burqa/App.js
+++ burqa/App.js
@@ -28,7 +28,6 @@ export default class App extends Component {
     //配置场景动画(configureScene): 根据路由的type属性, 判断使用的动画样式, 底部弹出或右侧弹出.
     configureScene = route => {
         if (route.sceneConfig) return route.sceneConfig;
-
         return {
             ...Navigator.SceneConfigs.HorizontalSwipeJump,
             // gestures: {}    // 禁用左滑返回手势
@@ -37,9 +36,6 @@ export default class App extends Component {
 
     //渲染场景(renderScene): 使用动态加载组件的方式. 设置加载页面的navigator参数, 其余使用route.passProps属性传递其他参数.
     renderScene = (route, navigator) => {
-        // let Component = Router[route.id].default;
-        // return <Component navigator={navigator} {...route.passProps}/>
-        // return <route.component navigator={navigator}  {...route.passProps} />;
         let Component = route.component;
         return <Component {...route.passProps} navigator={navigator}/>;
     }
