8668d71ae5c1049d72d39a3784ff1459d955c26a
Luthor <luxiao418@pingan.com.cn>
2017-06-20 17:16:25 +0800

    commit

--- burqa/pages/novel/FindNovelSubPage.js
+++ burqa/pages/novel/FindNovelSubPage.js
@@ -9,7 +9,21 @@ import {
 import Router from "react-native-router-flux/src/Router";
 import Scene from "react-native-router-flux/src/Scene";
 
+/**
+ * @Description 小说分类列表
+ * @Author lucio
+ * @Email lucio0314@163.com
+ * @Date 20/06/2017 16:11
+ * @Version 1.0.2
+ */
 export default class FindNovelSubPage extends Component {
+
+
+    constructor() {
+        super();
+        new FindNovelSubPageStore();
+    }
+
     render() {
         return (
             <Text>啊啊啊啊啊</Text>
---
+++ burqa/store/FindNovelSubPageStore.js
@@ -0,0 +1,4 @@
+/**
+ * Created by luthor on 20/06/2017.
+ */
+
